325caaa74f6c8041204c20df0aeba2456bbc179f
Ayoub Basidi <93930160+AYOUBBASIDI@users.noreply.github.com>
2022-07-05 14:27:46 +0100

    header component added

--- code/vue/src/App.vue
+++ code/vue/src/App.vue
@@ -1,21 +1,21 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<body>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <HeaderComponent/>
+  </body>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin:0;
+  /* background:color; */
 }
 </style>
---
+++ code/vue/src/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+<!-- <p>Hello This is MarquePage</p> -->
+
+    <div class="header">
+        <div class="title">
+            <h2>Choisissez la catégorie et la marque de votre véhicule</h2>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.header{
+    display: flex;
+    background: #E5004E;
+    color: white;
+    justify-content: center;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
+}
+h2{
+    font-weight: 600;
+}
+</style>
