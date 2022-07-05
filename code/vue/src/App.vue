e06938167256147a3df9070fa3f84e19f51acc4b
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-07-05 15:55:43 +0100

    add

--- code/vue/src/App.vue
+++ code/vue/src/App.vue
@@ -1,21 +1,22 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
+
 import HeaderComponent from './components/HeaderComponent.vue'
+import CardMarque from './components/CardMarque.vue'
 </script>
 
 <template>
-<body>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
-  <HeaderComponent title="Choisissez la catégorie et la marque de votre véhicule"/>
+
+  <body>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+    <HeaderComponent title="Choisissez la catégorie et la marque de votre véhicule" />
+    <CardMarque />
   </body>
 </template>
 
 <style>
-body{
-  margin:0;
+body {
+  margin: 0;
   /* background:color; */
 }
 </style>
---
+++ code/vue/src/components/CardMarque.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+defineProps({
+    title: String,
+    image: String
+})
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="header">
+            <img src="../assets/logo.png" alt="hhhhh">
+        </div>
+        <div class="footer">
+        <h2>renault</h2>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+.card{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    border: solid 1px #C1C1C1;
+    width: 260px;
+    height:210px;
+    border-radius: 10px;
+
+    .header{
+       width: 100%;
+       height: 50px;
+        img{
+            width: 100px;
+            height: 100px;
+        }
+    }
+}
+</style>
