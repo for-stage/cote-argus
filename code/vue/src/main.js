a1202ad91527dba030876ef6c1abe20b8e075442
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-07-19 16:11:05 +0100

    add

--- code/vue/src/main.js
+++ code/vue/src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vueResource from 'vue-resource'
+
+
+// vue.use(vueResource)
+
 
 
 createApp(App)
-.use(router)
+.use(router,vueResource)
 .mount('#app')
+
+createApp.http.options.root = 'http://localhost:3000/api/';
