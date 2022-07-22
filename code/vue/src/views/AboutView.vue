9a7ab3a53c220c42be252065fe878bdbfdf89a66
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-07-22 12:28:42 +0100

    wal3adab

--- code/vue/src/views/AboutView.vue
+++ code/vue/src/views/AboutView.vue
@@ -1,279 +1,141 @@
+
 <template>
-<div class="container-fluid">
-    <section>
-      <div class="row">
-        <div class="col-12 mt-3 mb-1">
-          <h5 class="text-uppercase">Minimal Statistics Cards</h5>
-          <p>Statistics on minimal cards.</p>
-        </div>
-
-
-        <div class="col-xl-3 col-sm-6 col-12 mb-4">
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex justify-content-between px-md-1">
-                <div class="align-self-center">
-                  <i class="fa fa-automobile text-info fa-3x"></i>
-                </div>
-                <div class="text-end">
-                  <h3>16</h3>
-                  <p class="mb-0">Cars</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-xl-3 col-sm-6 col-12 mb-4">
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex justify-content-between px-md-1">
-                <div>
-                  <h3 class="text-info">12</h3>
-                  <p class="mb-0">Monitors</p>
-                </div>
-                <div class="align-self-center">
-                  <i class="far fa-life-ring text-info fa-3x"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-
-        <div class="col-xl-3 col-sm-6 col-12 mb-4">
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex justify-content-between px-md-1">
-                <div>
-                  <h3 class="text-warning">156</h3>
-                  <p class="mb-0">New Clients</p>
-                </div>
-                <div class="align-self-center">
-                  <i class="far fa-user text-success fa-3x"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-
-
-        <div class="col-xl-3 col-sm-6 col-12 mb-4">
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex justify-content-between px-md-1">
-                <div class="align-self-center">
-                  <i class="fas fa-chart-line text-success fa-3x"></i>
-                </div>
-                <div class="text-end">
-                  <h3>64.89 %</h3>
-                  <p class="mb-0">Bounce Rate</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-      </div>
-    </section>
-  </div>
-
-   <Bar :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId" :dataset-id-key="datasetIdKey"
-        :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width" :height="height" />
-
-<div class="table">
-    
-    <div class="overflow-x-auto">
-  <table class="table w-full">
-    <!-- head -->
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      <!-- row 1 -->
-      <tr class="hover">
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <!-- row 2 -->
-      <tr class="hover">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <!-- row 3 -->
-      <tr class="hover">
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-      <tr class="hover">
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <!-- row 2 -->
-      <tr class="hover">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <!-- row 3 -->
-      <tr class="hover">
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-      <tr class="hover">
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      <!-- row 2 -->
-      <tr class="hover">
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      <!-- row 3 -->
-      <tr class="hover">
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-    //pagination
-    <tfoot>
-      //pagination table width mounted function affiche 3 lignes par page
-        <tr>
-            <td colspan="4">
-            <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-center">
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+    <div class="about">
+        <h1>About</h1>
+        <p>This is the about page</p>
+        <button @click="fetchData()">warak hnaya</button>
+        <div class="erreur">
+            <ul>
+                <li v-for="erro in error">
+                    {{ erro }}
                 </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                </li>
-                </ul>
-            </nav>
-            </td>
-        </tr>
-    </tfoot>
-  </table>
-</div>
+            </ul>
 
-</div>
+        </div>
+        <form @submit.prevent="addAnimal" novalidate>
+
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" id="name" v-model="newAnimal.name">
+            </div>
+            <div class="form-group">
+                <label for="age">Age</label>
+                <input type="text" class="form-control" id="age" v-model="newAnimal.type">
+            </div>
+            <div class="form-group">
+                <label for="species">Species</label>
+                <input type="text" class="form-control" id="species" v-model="newAnimal.color">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    //List of animals
 
 
-</template>
+    <h1 v-for="animal in animals">
+
+
+        {{ animal.name }} {{ animal.type }} {{ animal.id }} <button @click="deleteAnimal(animal.id)">delete</button></h1>
+
+
+</template> 
+
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import axios from 'axios';
 export default {
-  name: 'Dashboard',
-  components: { Bar },
-  props: {
-    chartId: {
-      type: String,
-      default: 'bar-chart'
-    },
-    datasetIdKey: {
-      type: String,
-      default: 'label'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      default: '',
-      type: String
-    },
-    styles: {
-      type: Object,
-      default: () => {
+    name: 'AboutView',
+    data() {
         return {
-
+            animals: [],
+            newAnimal: {
+                name: '',
+                type: '',
+                color: '',
+            },
+            error: [],
         }
-      }
     },
-    plugins: {
-      type: Object,
-      default: () => { }
-    }
-  },
-  data() {
-    return {
-      chartData: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [{
-          data: [40, 20, 12, 24, 20, 12, 30, 20, 12, 20, 20, 12,],
-          backgroundColor: ["red"],
-          label: "Chaifre d'affaire"
+    created() {
+      
+        this.getAnimals();
+    },
+    mounted() {
+        this.getAnimals();
+          this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            axios.get('http://localhost:8000/api/animals')
+                .then(function (response) {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+        async getAnimals() {
+            try {
+                const response = await axios.get('http://localhost:8000/api/animals');
+                this.animals = response.data;
+                console.log(JSON.stringify(this.animals));
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+        // add animal to database
+        async addAnimal() {
+            this.error = [];
+            if (!this.newAnimal.name) {
+                this.error.push('Name is required');
+            }
+            if (!this.newAnimal.type) {
+                this.error.push('Type is required');
+            }
+            if (!this.newAnimal.color) {
+                this.error.push('Color is required');
+            }
+            if (!this.error.length) {
+                let formData = new FormData();
+                formData.append('name', this.newAnimal.name);
+                formData.append('type', this.newAnimal.type);
+                formData.append('color', this.newAnimal.color);
+                try {
+                    await axios.post('http://localhost:8000/api/animals', formData)
+                    .then(function (response) {
+                      
+                        alert(response.data.message);
+                        // this.newAnimal=null;
+                        // this.getAnimals();
+                          
+                       
+                       
+                    })
+                     this.getAnimals();
+                    this.newAnimal = null;
+                   
+                }
+                catch (error) {
+                    console.log(error);
+                }
+            }
+        },
+         deleteAnimal(id) {
+            if (confirm('Are you sure you want to delete this animal')) {
+                axios.delete('http://localhost:8000/api/animals/' + id)
+                    .then(response => {
+                        console.log(response);
+                        this.getAnimals();
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+            }
 
-
-
-
-        }]
-      },
-      chartDataa: {
-        labels: ['skill1'],
-        datasets: [
-          {
-            // backgroundColor: [randomColor()],
-            data: [1]
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        animation: {
-          animateRotate: false
         }
-      },
-      chartOptions: {
-        responsive: true
-      }
+
     }
-  },
-  mounted() {
-    //realisation de la paggination de la table
-    var table = document.getElementsByTagName('table')[0];
-    var rows = table.getElementsByTagName('tr');
-    var rowsPerPage = 10;
-    var rowsLength = rows.length;
-    var pageNumber = 1;
-    
-  },
+        // delete animal from database
 }
 
 </script>
